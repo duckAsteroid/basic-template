9b4d9532f27e40c6fea0ac5ec7d76b4df1ac3127
Chris Senior <chris.senior@snapon.com>
2017-08-07 10:44:18 +0100

    First cut

---
+++ src/main/java/com/asteroid/duck/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.asteroid.duck.example;
+
+/**
+ * A sample hello world
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
